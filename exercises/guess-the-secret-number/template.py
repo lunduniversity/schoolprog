56a7804cede7dd1cf68afdeaadb87263f5261241
Simon Persson <simon@akep.se>
2017-12-13 09:57:15 +0100

    [guess-the-secret-number] added template

---
+++ exercises/guess-the-secret-number/template.py
@@ -0,0 +1,20 @@
+from random import randint
+
+min_number = ##
+max_number = ##
+
+random_number = randint(min_number, max_number)
+
+found = False
+
+while not found:
+    guess = int(input())
+    if ##:
+        print('Correct!')
+        found = True
+    elif ##:
+        print('higher')
+    elif ##:
+        print('lower')
+
+

b85b5befcf0a46b2c33efbc9a3715d6c8691cff3
Måns Magnusson <exoji2e@gmail.com>
2018-02-13 15:31:33 +0100

    add weatherdata gz-file, and fetch script

--- exercises/weatherdata/fetchdata.py
+++ exercises/weatherdata/fetchdata.py
@@ -1,11 +1,20 @@
 import sys
 from wrap_smhi_api import *
+from collections import defaultdict
 stations = get_stations()
 s = []
 for i, name in stations:
-    s.append("{} {}".format(i, name))
-    sys.stderr.write(s[-1] + '\n')
+    sys.stderr.write(name + '\n')
+    fst = True
+    data = list(get_data(i))
+    if len(data) < 20000: continue
     for (y, m, d), t in get_data(i):
+        if fst and y!=1961:
+            break
+        elif fst:
+            fst = False
+            s.append("{} {}".format(i, name))
         s.append("{} {} {} {}".format(y, m, d, t))
-    s.append('')
+    if not fst:
+        s.append('')
 print('\n'.join(s))
